8a487db191830b2ba9b2067d3de68ffa6778cb51
Vinicius Bellini <viniciusbellini@gmail.com>
2019-10-11 17:31:03 -0300

    CrossOrigin (CORS)

--- src/main/kotlin/com/bellini/pdimmixx/controller/UserController.kt
+++ src/main/kotlin/com/bellini/pdimmixx/controller/UserController.kt
@@ -3,6 +3,7 @@ package com.bellini.pdimmixx.controller
 import com.bellini.pdimmixx.model.User
 import com.bellini.pdimmixx.service.UserService
 import org.springframework.http.ResponseEntity
+import org.springframework.web.bind.annotation.CrossOrigin
 import org.springframework.web.bind.annotation.DeleteMapping
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PathVariable
@@ -13,6 +14,7 @@ import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
 
 @RestController
+@CrossOrigin(origins = ["*"])
 @RequestMapping("users")
 class UserController(private val userService: UserService) {
 
