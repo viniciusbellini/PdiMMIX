1b09259dd23a4ce9325286a17170e895efe25bdc
Vinicius Bellini <viniciusbellini@gmail.com>
2019-10-11 10:34:46 -0300

    Exclusão do modelo Note

--- src/main/kotlin/com/bellini/pdimmixx/repository/NoteRepository.kt
+++
@@ -1,6 +0,0 @@
-package com.bellini.pdimmixx.repository
-
-import com.bellini.pdimmixx.model.Note
-import org.springframework.data.repository.CrudRepository
-
-interface NoteRepository : CrudRepository<Note, Long>
