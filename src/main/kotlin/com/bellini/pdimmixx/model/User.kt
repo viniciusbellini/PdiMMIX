a674dab22f51141cdc71227035bd765ab25bbbef
Vinicius Bellini <viniciusbellini@gmail.com>
2019-10-21 08:14:54 -0300

    Adicionado email na classe User

--- src/main/kotlin/com/bellini/pdimmixx/model/User.kt
+++ src/main/kotlin/com/bellini/pdimmixx/model/User.kt
@@ -12,6 +12,7 @@ data class User(
         @JsonProperty("id", access = JsonProperty.Access.READ_ONLY)
         val id: Long = 0L,
         val name: String = "",
+        val email: String = "",
         val password: String = "",
         val isActive: Boolean = true
 )
